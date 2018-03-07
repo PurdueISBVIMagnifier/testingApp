5f5882bff9649e85ae67702cde370d1fd8b31779
Meriem <mbounab@purdue.edu>
2018-03-07 16:01:49 -0500

    cleaned up extraneous code

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -46,37 +46,7 @@ class ViewController: UIViewController {
         UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
     }
     
-    func screenshot() -> UIImage {
-        print("IM HERE")
-        let imageSize = UIScreen.main.bounds.size as CGSize;
-        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
-        let context = UIGraphicsGetCurrentContext()
-        for obj : AnyObject in UIApplication.shared.windows {
-            if let window = obj as? UIWindow {
-                if window.responds(to: #selector(getter: UIWindow.screen)) || window.screen == UIScreen.main {
-                    // so we must first apply the layer's geometry to the graphics context
-                    context!.saveGState();
-                    // Center the context around the window's anchor point
-                    context!.translateBy(x: window.center.x, y: window.center
-                        .y);
-                    // Apply the window's transform about the anchor point
-                    context!.concatenate(window.transform);
-                    // Offset by the portion of the bounds left of and above the anchor point
-                    context!.translateBy(x: -window.bounds.size.width * window.layer.anchorPoint.x,
-                                         y: -window.bounds.size.height * window.layer.anchorPoint.y);
-                    
-                    // Render the layer hierarchy to the current context
-                    window.layer.render(in: context!)
-                    
-                    // Restore the context
-                    context!.restoreGState();
-                }
-            }
-        }
-        let image = UIGraphicsGetImageFromCurrentImageContext();
-        
-        return image!
-    }
+    
 
 
 }
