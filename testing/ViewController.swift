a18fb8166023c1fb2d21d122ba3c8481c2554351
Meriem <mbounab@purdue.edu>
2018-03-07 15:41:30 -0500

    added the screenshot button and added functionality

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var webViewTest: UIWebView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +27,55 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func takeScreenshot(_ sender: Any) {
+        
+        captureScreenshot()
+    }
+    func captureScreenshot(){
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        // Creates UIImage of same size as view
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        // THIS IS TO SAVE SCREENSHOT TO PHOTOS
+        UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+    }
+    
+    func screenshot() -> UIImage {
+        print("IM HERE")
+        let imageSize = UIScreen.main.bounds.size as CGSize;
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        for obj : AnyObject in UIApplication.shared.windows {
+            if let window = obj as? UIWindow {
+                if window.responds(to: #selector(getter: UIWindow.screen)) || window.screen == UIScreen.main {
+                    // so we must first apply the layer's geometry to the graphics context
+                    context!.saveGState();
+                    // Center the context around the window's anchor point
+                    context!.translateBy(x: window.center.x, y: window.center
+                        .y);
+                    // Apply the window's transform about the anchor point
+                    context!.concatenate(window.transform);
+                    // Offset by the portion of the bounds left of and above the anchor point
+                    context!.translateBy(x: -window.bounds.size.width * window.layer.anchorPoint.x,
+                                         y: -window.bounds.size.height * window.layer.anchorPoint.y);
+                    
+                    // Render the layer hierarchy to the current context
+                    window.layer.render(in: context!)
+                    
+                    // Restore the context
+                    context!.restoreGState();
+                }
+            }
+        }
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        
+        return image!
     }
 
 
