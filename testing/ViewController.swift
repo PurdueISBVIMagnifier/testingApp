3d951ef479b18934232204d71de0d222fa5ad457
Nicholas Rosato <nicholasrosato@Nicholass-MacBook-Pro.local>
2018-03-08 16:21:02 -0500

    added animation for screen shot

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -17,10 +17,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //Sets URL string
-        let url = NSURL (string: "http://128.46.121.195:8081/");
-        
+       
         //IP ADDRESSES USED
-        //192.168.1.4:8081  //128.46.121.195:8081 //128.46.121.195:8081 //10.160.165.62:8081
+        //192.168.1.4:8081  //128.46.121.195:8081 //128.46.121.195:8081 //10.160.165.62:8081  //128.211.222.119:8081
+        let url = NSURL (string: "http://128.211.222.119:8081/");
         
         //Makes http Request
         let request = NSURLRequest(url: url! as URL);
@@ -45,6 +45,7 @@ class ViewController: UIViewController {
     
     func captureScreenshot()
     {
+        showScreenshotEffect()
         let layer = UIApplication.shared.keyWindow!.layer
         let scale = UIScreen.main.scale
       
@@ -56,6 +57,8 @@ class ViewController: UIViewController {
         let screenshot = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+        
+        
     }
     
     //DOES NOT WORK YET
@@ -66,6 +69,30 @@ class ViewController: UIViewController {
         UIApplication.shared.open(URL(string:"App-prefs:root=General&path=ACCESSIBILITY")!)
     }
     
+    //Animation for Screen Shot
+    func showScreenshotEffect() {
+        
+        let snapshotView = UIView()
+        snapshotView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(snapshotView)
+        
+        let constraints:[NSLayoutConstraint] = [
+            snapshotView.topAnchor.constraint(equalTo: view.topAnchor),
+            snapshotView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            snapshotView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            snapshotView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+        snapshotView.backgroundColor = UIColor.white
+        
+        UIView.animate(withDuration: 0.6, animations: {
+            snapshotView.alpha = 0
+        }) { _ in
+            snapshotView.removeFromSuperview()
+        }
+    }
+
     
 
 
